7d79faf24ca2a08d6acfcf9e752598cc64a39202
杨晓磊 <1414225@qq.com>
2018-08-05 20:50:11 +0800

    Initial commit

---
+++ src/main/java/net/dowhile/justDoIt/JustDoItApplication.java
@@ -0,0 +1,12 @@
+package net.dowhile.justDoIt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JustDoItApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JustDoItApplication.class, args);
+	}
+}
